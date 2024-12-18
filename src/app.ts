a9412b97d589f6db8782ec738950f9d8279999db
R4V3NSH4D0W <lenishmagar@gmail.com>
2024-12-18 16:50:00 +0545

    testing

--- src/app.ts
+++ src/app.ts
@@ -2,19 +2,20 @@ import express from "express";
 import gogoRoute from './routes/gogo-route';
 import zoroRoute from './routes/zoro-route';
 import _9animeRoute from './routes/9anime-route';
-const path = require('path');
+import path from 'path';
+
 const app = express();
 
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '../public')));
 
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
-  });
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
-app.use('/api/gogoanime',gogoRoute)
-app.use('/api/zoroanime',zoroRoute)
-app.use('/api/9anime',_9animeRoute)
+app.use('/api/gogoanime', gogoRoute);
+app.use('/api/zoroanime', zoroRoute);
+app.use('/api/9anime', _9animeRoute);
 
 export default app;
