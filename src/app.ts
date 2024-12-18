04e6a971873a1c0b607a5229dc97fabd0412992b
R4V3NSH4D0W <lenishmagar@gmail.com>
2024-12-18 16:05:07 +0545

    added api doc

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,16 @@ import express from "express";
 import gogoRoute from './routes/gogo-route';
 import zoroRoute from './routes/zoro-route';
 import _9animeRoute from './routes/9anime-route';
+const path = require('path');
 const app = express();
 
 app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'apiInfo.html'));
+});
+console.log(path.join(__dirname, 'apiInfo.html'));
+
 
 app.use('/api/gogoanime',gogoRoute)
 app.use('/api/zoroanime',zoroRoute)
